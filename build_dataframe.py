7495fb82397508eb1d8634c7ecee5667c168c51c
Mikko Kotila <mailme@mikkokotila.com>
2021-06-18 19:38:23 +0300

    Create build_dataframe.py

---
+++ build_dataframe.py
@@ -0,0 +1,31 @@
+import zipfile
+from bs4 import BeautifulSoup
+url = '''https://glossaries.dila.edu.tw/data/mahavyutpatti.dila.tei.p5.xml.zip'''
+with zipfile.ZipFile(url, 'r') as zip_ref:
+    zip_ref.extractall(data)
+
+import pandas as pd
+
+with open('data/mahavyutpatti.dila.tei.p5.xml', 'r') as f:
+    data = f.read()
+print("1")
+bs_data = BeautifulSoup(data, "xml")
+print("2")
+data = bs_data.find_all('entry')
+
+out = {}
+print("3")
+for i in range(len(data)):
+
+    skt_str = data[i].find('orth', {'xml:lang':'san-Latn'}).text
+    tib_str = data[i].find('cit', {'xml:lang':'bod-Tibt'}).text.strip()
+
+    out[tib_str] = skt_str
+
+out = pd.DataFrame.from_dict(out, 'index').reset_index()
+out.columns = ['Tibetan-Word', 'Sanskrit-Word']
+
+out['Tibetan-Word'] = out['Tibetan-Word'].str.strip()
+out['Sanskrit-Word'] = out['Sanskrit-Word'].str.strip()
+
+return out
